1b0b54b86fef32f1849dff69a0cda6b8836ca44e
Abhishek Kumar <abhishekk580@gmail.com>
2021-10-21 14:53:14 +0530

    added django debug toolbar

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'playground'
+    'playground',
+
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'storefront.urls'
--- storefront/urls.py
+++ storefront/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include, include
+from django.urls import path, include
+
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
+
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
